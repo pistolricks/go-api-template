beb4b14d0ca7bd65a0e2953e582da0372499594e
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-31 00:57:46 -0800

    removed the vendor module

--- internal/extended/extended.go
+++ internal/extended/extended.go
@@ -11,13 +11,11 @@ var (
 )
 
 type Extended struct {
-	Vendors  VendorModel
 	Contents ContentModel
 }
 
 func NewExtended(db *sql.DB) Extended {
 	return Extended{
-		Vendors:  VendorModel{DB: db},
 		Contents: ContentModel{DB: db},
 	}
 }
--- migrations/000002_add_vendors_check_constraints.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE vendors DROP CONSTRAINT IF EXISTS vendors_runtime_check;
-
-ALTER TABLE vendors DROP CONSTRAINT IF EXISTS vendors_year_check;
-
-ALTER TABLE vendors DROP CONSTRAINT IF EXISTS genres_length_check;
